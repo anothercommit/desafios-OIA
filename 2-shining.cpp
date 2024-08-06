81bd9e810d7076261ccaf1e3f1957d599a98a9c4
anothercommit <lack_of_names@proton.me>
2024-08-06 19:40:19 -0300

    Tue Aug  6 07:40:19 PM -03 2024

--- 2-shining.cpp
+++ 2-shining.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#define posicion(i) (map[fila[(i)]][col[(i)]])
+#define posicion(i) (mapa[fila[(i)]][col[(i)]])
 
 using namespace std;
 
@@ -8,21 +8,21 @@ int sanar(vector<int>, vector<int>, vector<int>, vector<int>, int,
           vector<vector<char>>);
 
 int main() {
-  vector<int> fila = {0, 2};
-  vector<int> col = {1, 1};
-  vector<int> vidaInicial = {10, 20};
-  vector<int> vidaActual = {6, 10};
-  int d = 3;
-  vector<vector<char>> map = {
+  vector<int> fila = {1, 1};
+  vector<int> col = {0, 2};
+  // vector<int> vidaInicial = {10, 20};
+  // vector<int> vidaActual = {6, 10};
+  // int d = 3;
+  vector<vector<char>> mapa = {
       {'.', 'X', '.'}, {'.', 'X', '.'}, {'.', '.', '.'}};
 
-  cout << sanar(fila, col, vidaInicial, vidaActual, d, map);
-
+  cout << posicion(0) << '\n';
+  // sanar(fila, col, vidaInicial, vidaActual, d, mapa);
   return 0;
 }
 
 int sanar(vector<int> fila, vector<int> col, vector<int> vidaInicial,
-          vector<int> vidaActual, int d, vector<vector<char>> map) {
+          vector<int> vidaActual, int d, vector<vector<char>> mapa) {
 
   if (posicion(0) == '.')
     return 1;
