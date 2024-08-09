3c957335a7c6680eebd1fb6fd5a2363f6625452b
anothercommit <lack_of_names@proton.me>
2024-08-08 22:04:50 -0300

    Thu Aug  8 10:04:50 PM -03 2024

--- Desafío 2/2-shining.cpp
+++ Desafío 2/2-shining.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 #define POSICION(i) mapa[fila[i]][col[i]]
 
 using namespace std;
 
-int sanar(vector<int>, vector<int>, vector<int>, vector<int>, int,
-          vector<vector<char>>);
-void printMapa(vector<vector<char>>);
+int sanar(vector<int> &, vector<int> &, vector<int> &, vector<int> &, int,
+          vector<vector<string>> &);
+string printMapa(const vector<vector<string>> &);
 
 int main() {
   vector<int> fila;
   vector<int> col;
   vector<int> vidaInicial;
   vector<int> vidaActual;
-  vector<vector<char>> mapa;
+  vector<vector<string>> mapa;
 
   int n, r, c, d, num;
   cin >> n >> r >> c;
@@ -28,17 +29,18 @@ int main() {
 
   cin >> d;
 
-  char ch;
   for (int y = 0; y < r; ++y) {
     mapa.push_back({});
-    for (int x = 0; x < c; ++x)
-      mapa[y].push_back((cin >> ch, ch));
+    for (int x = 0; x < c; ++x) {
+      char c = (cin >> c, c);
+      mapa[y].push_back(c != 'X' ? "-1" : string(1, c));
+    }
   }
 
   for (int i = 1; i < col.size(); ++i)
-    POSICION(i) = 'L';
+    POSICION(i) = "L";
 
-  printMapa(mapa);
+  // cout << printMapa(mapa);
 
   cout << sanar(fila, col, vidaInicial, vidaActual, d, mapa) << '\n';
 
@@ -46,57 +48,66 @@ int main() {
 }
 
 int sanar(vector<int> &fila, vector<int> &col, vector<int> &vidaInicial,
-          vector<int> &vidaActual, int d, vector<vector<char>> &mapa) {
+          vector<int> &vidaActual, int d, vector<vector<string>> &mapa) {
   if (0 > fila[0] || fila[0] >= mapa.size() || 0 > col[0] ||
-      col[0] >= mapa.size())
+      col[0] >= mapa[fila[0]].size())
     return 0;
-  else if (POSICION(0) == 'X' || d < 0)
+  else if (POSICION(0) == "X" || (d == 0 && POSICION(0) == "L"))
+    return 0;
+  else if (POSICION(0) != "L" && stoi(POSICION(0)) >= d)
     return 0;
 
-  POSICION(0) = 'X';
-  cout << d << endl;
-  printMapa(mapa);
-  int maxHeal = 0;
+  POSICION(0) = "+";
+  cout << printMapa(mapa) << endl;
+  POSICION(0) = to_string(d);
+  // int maxHeal = 0;
 
-  if (POSICION(0) != 'L')
-    for (int i = 0, size = col.size(); i < size; ++i)
-      if ((abs(fila[0] - fila[i]) + abs(col[0] - col[i])) <= 2) {
-        int posibleCuracion = vidaInicial[i] - vidaActual[i];
+  // if (POSICION(0) != 'L')
+  //   for (int i = 0, size = col.size(); i < size; ++i)
+  //     if ((abs(fila[0] - fila[i]) + abs(col[0] - col[i])) <= 2) {
+  //       int posibleCuracion = vidaInicial[i] - vidaActual[i];
 
-        posibleCuracion = posibleCuracion > 10 ? 10 : posibleCuracion;
-        maxHeal = posibleCuracion > maxHeal ? posibleCuracion : maxHeal;
-      }
+  //       posibleCuracion = posibleCuracion > 10 ? 10 : posibleCuracion;
+  //       maxHeal = posibleCuracion > maxHeal ? posibleCuracion : maxHeal;
+  //     }
+
+  if (d <= 0)
+    return 0;
 
   fila[0] -= 1;
   int arriba = sanar(fila, col, vidaInicial, vidaActual, d - 1, mapa);
-  if (maxHeal < arriba)
-    maxHeal = arriba;
+  // if (maxHeal < arriba)
+  //   maxHeal = arriba;
 
   fila[0] += 1;
   col[0] += 1;
   int derecha = sanar(fila, col, vidaInicial, vidaActual, d - 1, mapa);
-  if (derecha > maxHeal)
-    maxHeal = derecha;
+  // if (derecha > maxHeal)
+  //   maxHeal = derecha;
 
   col[0] -= 1;
   fila[0] += 1;
   int abajo = sanar(fila, col, vidaInicial, vidaActual, d - 1, mapa);
-  if (abajo > maxHeal)
-    maxHeal = abajo;
+  // if (abajo > maxHeal)
+  //   maxHeal = abajo;
 
   fila[0] -= 1;
   col[0] -= 1;
   int izquierda = sanar(fila, col, vidaInicial, vidaActual, d - 1, mapa);
-  if (maxHeal < izquierda)
-    maxHeal = izquierda;
+  // if (maxHeal < izquierda)
+  //   maxHeal = izquierda;
 
-  return maxHeal;
+  // return maxHeal;
+  return 0;
 }
 
-void printMapa(vector<vector<char>> mapa) {
+string printMapa(const vector<vector<string>> &mapa) {
+  string m;
+
   for (int f = 0, size = mapa.size(); f < size; ++f) {
     for (int c = 0, size = mapa[f].size(); c < size; ++c)
-      cout << mapa[f][c] << ' ';
-    cout << '\n';
+      m += mapa[f][c] + "  ";
+    m += '\n';
   }
+  return m;
 }
